9dccb42aa60e969f975d3b8f345bc340a2d5d239
rafsun dipto <rafsundipto116@gmail.com>
2026-04-13 14:00:12 +0600

    theme and navbar added

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,193 @@
+"use client";
+
+import { Book, Menu, Pill, Sunset, Trees, Zap } from "lucide-react";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+
+interface MenuItem {
+  title: string;
+  url: string;
+  description?: string;
+  icon?: React.ReactNode;
+  items?: MenuItem[];
+}
+
+interface Navbar1Props {
+  className?: string;
+  logo?: {
+    url: string;
+    src: string;
+    alt: string;
+    title: string;
+    className?: string;
+  };
+  menu?: MenuItem[];
+  auth?: {
+    login: {
+      title: string;
+      url: string;
+    };
+    signup: {
+      title: string;
+      url: string;
+    };
+  };
+}
+
+const Navbar = ({
+  logo = {
+    url: "/",
+    src: "",
+    alt: "MediStore logo",
+    title: "MediStore",
+  },
+  menu = [
+    { title: "Home", url: "/" },
+    { title: "Products", url: "/category" },
+    { title: "Shop", url: "/medicine" },
+    { title: "About", url: "/about" },
+
+  ],
+  auth = {
+    login: { title: "Login", url: "/logIn" },
+    signup: { title: "Sign up", url: "/register" },
+  },
+  className,
+}: Navbar1Props) => {
+  return (
+    <section className={cn(className)}>
+      <div className="container w-full mx-auto ">
+        {/* Desktop Menu */}
+        <nav className="hidden items-center justify-between py-4 lg:flex">
+          <div className="flex items-center gap-8">
+            {/* Logo */}
+            <a href={logo.url} className="flex items-center gap-2">
+              <Pill className="size-8 text-primary" />
+              <span className="text-2xl font-bold tracking-tight">
+                {logo.title}
+              </span>
+            </a>
+            <div className="flex items-center ">
+              <NavigationMenu>
+                <NavigationMenuList>
+                  {menu.map((item) => renderMenuItem(item))}
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+          </div>
+          <div className="flex gap-3">
+            <Button asChild variant="outline" size="lg">
+              <Link href={auth.login.url}>{auth.login.title}</Link>
+            </Button>
+            <Button asChild size="lg">
+              <Link href={auth.signup.url}>{auth.signup.title}</Link>
+            </Button>
+            <ModeToggle />
+          </div>
+        </nav>
+
+        {/* Mobile Menu */}
+        <div className="block lg:hidden">
+          <div className="flex items-center justify-between py-4">
+            {/* Logo */}
+            <a href={logo.url} className="flex items-center gap-2">
+              <Pill className="size-7 text-primary" />
+              <span className="text-xl font-bold tracking-tight">
+                {logo.title}
+              </span>
+            </a>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Menu className="size-5" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="overflow-y-auto">
+                <SheetHeader>
+                  <SheetTitle>
+                    <a href={logo.url} className="flex items-center gap-2">
+                      <Pill className="size-6 text-primary" />
+                      <span className="text-lg font-bold tracking-tight">
+                        {logo.title}
+                      </span>
+                    </a>
+                  </SheetTitle>
+                </SheetHeader>
+                <div className="flex flex-col gap-6 p-4">
+                  <Accordion
+                    type="single"
+                    collapsible
+                    className="flex w-full flex-col gap-4"
+                  >
+                    {menu.map((item) => renderMobileMenuItem(item))}
+                  </Accordion>
+
+                  <div className="flex flex-col gap-5">
+                    <Button asChild variant="outline">
+                      <Link href={auth.login.url}>{auth.login.title}</Link>
+                    </Button>
+                    <Button asChild>
+                      <Link href={auth.signup.url}>{auth.signup.title}</Link>
+                    </Button>
+                  </div>
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const renderMenuItem = (item: MenuItem) => {
+
+
+  return (
+    <NavigationMenuItem key={item.title}>
+      <NavigationMenuLink
+        asChild
+        className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-lg font-medium transition-colors hover:bg-muted hover:text-accent-foreground"
+      >
+        <Link href={item.url}>{item.title}</Link>
+      </NavigationMenuLink>
+    </NavigationMenuItem>
+  );
+};
+
+const renderMobileMenuItem = (item: MenuItem) => {
+
+
+  return (
+    <Link key={item.title} href={item.url} className="text-md font-semibold">
+      {item.title}
+    </Link>
+  );
+};
+
+
+export { Navbar };
